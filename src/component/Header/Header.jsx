ac959a7d9fd36e6d06153b852fe950b6e0e1e13c
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2024-07-11 22:12:16 +0600

    Header section made responsive

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,73 +4,143 @@ import { NavLink } from 'react-router-dom';
 import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
-    const [open, isOpen] = useState(false)
+    const [fold, setFold] = useState(false)
     return (
-        <nav className='flex items-center justify-between p-6 bg-blue-300'>
-            {/* <h1 className='text-2xl text-blue-800 font-extrabold'>GeoReligivity</h1> */}
-            <NavLink to="/">
-                <img className='w-16' src="../../../public/logo2.png" alt="" />
-            </NavLink>
-            {/* <div className='md:hidden'>
-                <span className='w-min'>
-                    {open ?
-                        <button className='w-min' onClick={() => isOpen(!open)}>
-                            <XMarkIcon className="size-10 text-purple-700 hover:text-purple-900" />
-                        </button>
-                        :
-                        <button className='w-min' onClick={() => isOpen(!open)}>
-                            <Bars3BottomRightIcon className="size-10 text-purple-700 hover:text-purple-900" />
-                        </button>}
-                </span>
-            </div> */}
+        <div className='px-4 md:px-24 lg:px-8 sm:max-w-xl  md:max-w-full lg:max-w-screen-2xl mx-auto'>
+            <nav className='relative flex items-center justify-between bg- p-6'>
+                {/* Logo section */}
+                <NavLink to="/">
+                    <img className='w-14' src="../../../public/logo2.png" alt="" />
+                </NavLink>
 
-
-            <ul className='md:flex gap-3 items-center'>
-                {/*  className='flex gap-3 items-center' */}
-                {/*  className={`bg-purple-400 p-4 md:flex absolute md:static duration-500 ${open ? 'top-14' : '-top-52'}`} */}
-
-                <div>
-                    <NavLink className={`block text-lg font-medium text-blue-800 ({ isActive }) =>
+                {/* Nav bar Itemm */}
+                <ul className='hidden lg:flex gap-3 items-center'>
+                    <li>
+                        <NavLink className={`block text-lg font-medium text-blue-700 ({ isActive }) =>
                     isActive
                         ? "active" : ""`} to="/">Home</NavLink>
-                </div>
-
-                <div>
-                    <NavLink className={`block text-lg font-medium text-blue-800 ({ isActive }) =>
+                    </li>
+                    <li>
+                        <NavLink className={`text-lg font-medium text-blue-700 ({ isActive }) =>
                     isActive
                         ? "active" : ""`} to="/media">Media</NavLink>
-                </div>
-
-                <div>
-                    <NavLink className={`block text-lg font-medium text-blue-800 ({ isActive }) =>
+                    </li>
+                    <li>
+                        <NavLink className={`text-lg font-medium text-blue-700 ({ isActive }) =>
                     isActive
                         ? "active" : ""`} to="/history">History</NavLink>
-                </div>
-
-                <div>
-                    <NavLink className={`block text-lg font-medium text-blue-800 ({ isActive }) =>
+                    </li>
+                    <li>
+                        <NavLink className={`text-lg font-medium text-blue-700 ({ isActive }) =>
                     isActive
                         ? "active" : ""`} to="hidden-pearls">Hidden Pearls</NavLink>
-                </div>
+                    </li>
 
-                <div>
-                    <NavLink className={`block text-lg font-medium text-blue-800 ({ isActive }) =>
+                    <li>
+                        <NavLink className={`text-lg font-medium text-blue-700 ({ isActive }) =>
                     isActive
                         ? "active" : ""`} to="/parenting">Parenting</NavLink>
-                </div>
-
-                <div>
-                    <NavLink className={`block text-lg font-medium text-blue-800 ({ isActive }) =>
+                    </li>
+                    <li>
+                        <NavLink className={`text-lg font-medium text-blue-800 ({ isActive }) =>
                     isActive
                         ? "active" : ""`} to="/series">Series
-                    </NavLink>
+                        </NavLink>
+                    </li>
+                </ul>
+                <div className='lg:hidden'>
+                    <button onClick={() => setFold(true)} className=''>
+                        <Bars3BottomRightIcon className='size-10' />
+                    </button>
+                    {
+                        fold && (
+                            <div className='absolute top-0 left-0 w-full z-10 justify-between'>
+                                <div className='bg-white p-6 border rounded-md shadow-xl'>
+                                    <div className='flex justify-between items-center'>
+                                        <div>
+                                            <NavLink to="/">
+                                                <img className='w-14' src="../../../public/logo2.png" alt="" />
+                                            </NavLink>
+                                        </div>
+                                        <div>
+                                            <button onClick={() => setFold(false)}>
+                                                <XMarkIcon className='size-10 text-red-600' />
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    {/* mobile nav bar */}
+                                    <nav>
+                                        <ul className=''>
+                                            <li className='text-lg font-medium text-blue-700'>
+                                                <NavLink className={({ isActive }) => isActive ? "active" : ""} to="/">Home</NavLink>
+                                            </li>
+                                            <li className='text-lg font-medium text-blue-700'>
+                                                <NavLink className={({ isActive }) => isActive ? "active" : ""} to="/media">Media</NavLink>
+                                            </li>
+                                            <li className='text-lg font-medium text-blue-700'>
+                                                <NavLink className={({ isActive }) => isActive ? "active" : ""} to="/history">History</NavLink>
+                                            </li>
+                                            <li className='text-lg font-medium text-blue-700'>
+                                                <NavLink className={({ isActive }) => isActive ? "active" : ""} to="hidden-pearls">Hidden Pearls</NavLink>
+                                            </li>
+
+                                            <li className='text-lg font-medium text-blue-700'>
+                                                <NavLink className={({ isActive }) => isActive ? "active" : ""} to="/parenting">Parenting</NavLink>
+                                            </li>
+                                            <li className='text-lg font-medium text-blue-700'>
+                                                <NavLink className={({ isActive }) => isActive ? "active" : ""} to="/series">Series
+                                                </NavLink>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
-            </ul>
-            {/* <div>
-                <input className='w-full rounded border border-blue-700 py-1' placeholder='Search' type="search" name="" id="" />
-            </div> */}
-        </nav>
+            </nav>
+        </div >
     );
 };
 
 export default Header;
+
+
+/*
+<ul className=''>
+                                        <div>
+                                            <NavLink className={`block text-lg font-medium text-blue-700 ({ isActive }) =>
+                    isActive
+                        ? "active" : ""`} to="/">Home</NavLink>
+                                        </div>
+                                        <div>
+                                            <NavLink className={`block text-lg font-medium text-blue-700 ({ isActive }) =>
+                    isActive
+                        ? "active" : ""`} to="/media">Media</NavLink>
+                                        </div>
+                                        <div>
+                                            <NavLink className={`block text-lg font-medium text-blue-700 ({ isActive }) =>
+                    isActive
+                        ? "active" : ""`} to="/history">History</NavLink>
+                                        </div>
+                                        <div>
+                                            <NavLink className={`block text-lg font-medium text-blue-700 ({ isActive }) =>
+                    isActive
+                        ? "active" : ""`} to="hidden-pearls">Hidden Pearls</NavLink>
+                                        </div>
+
+                                        <div>
+                                            <NavLink className={`block text-lg font-medium text-blue-700 ({ isActive }) =>
+                    isActive
+                        ? "active" : ""`} to="/parenting">Parenting</NavLink>
+                                        </div>
+                                        <div>
+                                            <NavLink className={`block text-lg font-medium text-blue-800 ({ isActive }) =>
+                    isActive
+                        ? "active" : ""`} to="/series">Series
+                                            </NavLink>
+                                        </div>
+                                    </ul>
+
+*/
